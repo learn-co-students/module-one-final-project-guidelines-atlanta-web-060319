d6602eb215aa03d427b8fa32c2f88a47772e64f6
MaryJay787 <josephmaryam.mj@gmail.com>
2019-06-18 10:15:37 -0400

    Done Adding Cli File

---
+++ app/cli.rb
@@ -0,0 +1,16 @@
+
+class CLI
+
+  def run
+    puts "Welcome to Hydrate Yourself"
+    puts "\n
+    MAIN MENU
+    \n"
+    puts "
+    1.Login
+    2.Take Quiz
+    3.Input Water Intake
+    \n"
+  end
+
+end
