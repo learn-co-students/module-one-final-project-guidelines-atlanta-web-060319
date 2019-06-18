037627e185a186985e3b032281184ca1f1505cfd
MaryJay787 <josephmaryam.mj@gmail.com>
2019-06-18 19:55:14 -0400

    Edits made to cli

--- app/cli.rb
+++ app/cli.rb
@@ -21,11 +21,11 @@ class CLI
 
   def main_menu
     choices = ["Take Quiz", "Input WaterIntake", "See Goal", "Update Profile", "Delete Account"]
-    a = @prompt.multi_select("Select Choice?", choices)
+    a = @prompt.select("Select Choice?", choices)
 
-    case choices
+    case a
       when choices[0]
-        take_quiz
+         take_quiz
       when choices[1]
         input_water_intake
       when choices[2]
@@ -35,7 +35,7 @@ class CLI
       when choices[4]
         delete_account
     end
-    binding.pry
+
   end
 
   def take_quiz
@@ -45,48 +45,49 @@ class CLI
     choices = ["One Cup Daily", "Three Cups Daily", "My Insides Are A Desert"]
     @prompt.multi_select("Select Amount?", choices)
 
-    # case
-    # when choices[0]
-    #   @counter += 1
-    # when choices[1]
-    #   @counter += 2
-    # when choices[2]
-    #   @counter += 3
-    # end
+    case
+    when choices[0]
+      @counter += 1
+    when choices[1]
+      @counter += 2
+    when choices[2]
+      @counter += 3
+      binding.pry
+    end
 
-    # puts " 2. How much water would you like to be drinking a day?"
-    # choices_two = ["Three Cups Daily", "Six Cups Daily", "Eight Cups Daily"]
-    # @prompt.multi_select("Select Amount?", choices_two)
-    #
-    # case
-    # when choices_two[0]
-    #   @counter += 1
-    # when choices_two[1]
-    #   @counter += 2
-    # when choices_two[2]
-    #   @counter += 3
-    # end
-    #
-    # puts " 3. What do you wish to gain from Hydrate Your Life?"
-    # choices_three = ["Better Health", "More Energy", "Finally Be Water Healthy"]
-    # @prompt.multi_select("Select Amount?", choices_three)
-    #
-    # case
-    # when choices_thre[0]
-    #   @counter += 1
-    # when choices_three[1]
-    #   @counter += 2
-    # when choices_three[2]
-    #   @counter += 3
-    # end
-    #
-    # if @counter == (1..4)
-    #   water = WaterIntake.create(:user_id @user.id, :goal_id @first.id, :tracker 0)
-    # elsif @counter == (5..6)
-    #   water = WaterIntake.create(:user_id @user.id, :goal_id @second.id, :tracker 0)
-    # else @counter == (7..10)
-    #   water = WaterIntake.create(:user_id @user.id, :goal_id @third.id, :tracker 0)
-    # end
+    puts " 2. How much water would you like to be drinking a day?"
+    choices_two = ["Three Cups Daily", "Six Cups Daily", "Eight Cups Daily"]
+    @prompt.multi_select("Select Amount?", choices_two)
+
+    case
+    when choices_two[0]
+      @counter += 1
+    when choices_two[1]
+      @counter += 2
+    when choices_two[2]
+      @counter += 3
+    end
+
+    puts " 3. What do you wish to gain from Hydrate Your Life?"
+    choices_three = ["Better Health", "More Energy", "Finally Be Water Healthy"]
+    @prompt.multi_select("Select Amount?", choices_three)
+
+    case
+    when choices_three[0]
+      @counter += 1
+    when choices_three[1]
+      @counter += 2
+    when choices_three[2]
+      @counter += 3
+    end
+
+    if @counter == (1..4)
+      water = WaterIntake.create(user_id: @user.id, goal_id: @first.id, tracker: 0)
+    elsif @counter == (5..6)
+      water = WaterIntake.create(user_id: @user.id, goal_id: @second.id, tracker: 0)
+    else @counter == (7..10)
+      water = WaterIntake.create(user_id: @user.id, goal_id: @third.id, tracker: 0)
+    end
   end
 
   def login
